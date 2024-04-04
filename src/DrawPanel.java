348ca33be0173314a1d0c4fa24b350f46f89d651
Shakib <shakiba4@nycstudents.net>
2024-04-04 13:14:23 -0400

    Made the "test your luck" button clickable

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -6,24 +6,36 @@ import javax.swing.JPanel;
 class DrawPanel extends JPanel implements MouseListener {
     private Rectangle button;
     public DrawPanel() {
-        button = new Rectangle(150, 300, 200, 30);
+        button = new Rectangle(375, 200, 200, 30);
         this.addMouseListener(this);
         this.setBackground(Color.BLACK);
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setFont(new Font("Courier New", Font.BOLD, 20));
+        g.setFont(new Font("Courier New", Font.PLAIN, 20));
+        g.setColor(Color.white);
         g.drawString("Test your luck!", 386, 221);
-        g.drawRect(375, 200, (int)button.getWidth(), (int)button.getHeight());
+        g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
     }
 
     public void mouseReleased(MouseEvent e) { }
     public void mouseEntered(MouseEvent e) { }
     public void mouseExited(MouseEvent e) { }
-    public void mouseClicked(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) {
+
+    }
 
     @Override
     public void mousePressed(MouseEvent e) {
+        Point clicked = e.getPoint();
+
+        if (e.getButton() == 1) {
+            if (button.contains(clicked)) {
+                System.out.println("Left Click");
+            } else if (!button.contains(clicked)) {
+                System.out.println("N");
+            }
+        }
     }
 }
