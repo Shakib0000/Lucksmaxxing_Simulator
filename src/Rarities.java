ae7ea3e5b0415c06e1b14b5cce2aae153339606a
Shakib <shakiba4@nycstudents.net>
2024-05-06 13:56:48 -0400

    Made rarities rarer, and started researching how to make animations.

--- src/Rarities.java
+++ src/Rarities.java
@@ -15,18 +15,18 @@ public class Rarities {
         rarities.add(new Rarity(408, 180, "Bronze", new Color(150, 75, 0), new Font("Century Schoolbook", Font.PLAIN, 40), 3));
         rarities.add(new Rarity(420, 180, "Silver", new Color(192, 192, 192), new Font("Century Schoolbook", Font.PLAIN, 40), 4));
         rarities.add(new Rarity(430, 180, "Gold", new Color(255, 215, 0), new Font("Century Schoolbook", Font.PLAIN, 40), 7));
-        rarities.add(new Rarity(388, 180, "Diamond", new Color(185, 242, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 20));
-        rarities.add(new Rarity(390, 180, "Emerald", new Color(20, 201, 81), new Font("Century Schoolbook", Font.PLAIN, 40), 45));
-        rarities.add(new Rarity(417, 180, "Topaz", new Color(255, 192, 103), new Font("Century Schoolbook", Font.PLAIN, 40), 55));
-        rarities.add(new Rarity(391, 180, "Sapphire", new Color(15,82,186), new Font("Century Schoolbook", Font.PLAIN, 40), 64));
-        rarities.add(new Rarity(429, 180, "Ruby", new Color(	224, 17, 95), new Font("Century Schoolbook", Font.PLAIN, 40), 88));
-        rarities.add(new Rarity(386, 180, "Amethyst", new Color(125, 0, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 100));
-        rarities.add(new Rarity(362, 180, "Aquamarine", new Color(	127, 255, 212), new Font("Century Schoolbook", Font.PLAIN, 40), 120));
-        rarities.add(new Rarity(432, 180, "Jade", new Color(3, 248, 162), new Font("Century Schoolbook", Font.PLAIN, 40), 144));
-        rarities.add(new Rarity(429, 180, "Opal", new Color(246, 149, 226), new Font("Century Schoolbook", Font.PLAIN, 40), 189));
-        rarities.add(new Rarity(389, 180, "Platinum", new Color(229,228,226), new Font("Century Schoolbook", Font.PLAIN, 40), 243));
-        rarities.add(new Rarity(387, 180, "Titanium", new Color(121, 121, 130), new Font("Century Schoolbook", Font.PLAIN, 40), 315));
-        rarities.add(new Rarity(376, 180, "Moonstone", new Color(190, 211, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 679));
+        rarities.add(new Rarity(388, 180, "Diamond", new Color(185, 242, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 60));
+        rarities.add(new Rarity(390, 180, "Emerald", new Color(20, 201, 81), new Font("Century Schoolbook", Font.PLAIN, 40), 105));
+        rarities.add(new Rarity(417, 180, "Topaz", new Color(255, 192, 103), new Font("Century Schoolbook", Font.PLAIN, 40), 125));
+        rarities.add(new Rarity(391, 180, "Sapphire", new Color(15,82,186), new Font("Century Schoolbook", Font.PLAIN, 40), 144));
+        rarities.add(new Rarity(429, 180, "Ruby", new Color(	224, 17, 95), new Font("Century Schoolbook", Font.PLAIN, 40), 188));
+        rarities.add(new Rarity(386, 180, "Amethyst", new Color(125, 0, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 220));
+        rarities.add(new Rarity(362, 180, "Aquamarine", new Color(	127, 255, 212), new Font("Century Schoolbook", Font.PLAIN, 40), 260));
+        rarities.add(new Rarity(432, 180, "Jade", new Color(3, 248, 162), new Font("Century Schoolbook", Font.PLAIN, 40), 310));
+        rarities.add(new Rarity(429, 180, "Opal", new Color(246, 149, 226), new Font("Century Schoolbook", Font.PLAIN, 40), 414));
+        rarities.add(new Rarity(389, 180, "Platinum", new Color(229,228,226), new Font("Century Schoolbook", Font.PLAIN, 40), 463));
+        rarities.add(new Rarity(387, 180, "Titanium", new Color(121, 121, 130), new Font("Century Schoolbook", Font.PLAIN, 40), 635));
+        rarities.add(new Rarity(376, 180, "Moonstone", new Color(190, 211, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 1499));
     }
 
     public Rarity generateRandomRarity(double luck) {
@@ -99,6 +99,8 @@ public class Rarities {
             return new String[0];
         }
     }
+
+
 
     public int getLuck() {
         return luck;
