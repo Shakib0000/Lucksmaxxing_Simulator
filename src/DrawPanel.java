4fc26efe99d3c92c28c51844aa33bda681024eed
Shakib <shakiba4@nycstudents.net>
2024-05-10 13:53:28 -0400

    Added a maximum capacity to luck and an indicator for when luck is maxed out
    Reworked how luck variable increases, the gameplay loop is more interesting now

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -13,6 +13,7 @@ class DrawPanel extends JPanel implements MouseListener {
     private int totalRollsForHighestRarity;
     private String[] rarityPercentages;
     private final int STARTING_SIMULATION_TIMES = 1000000;
+    private final int MAX_LUCK = 100;
 
     public DrawPanel() {
         testLuckButton = new Rectangle(375, 200, 200, 30);
@@ -38,8 +39,16 @@ class DrawPanel extends JPanel implements MouseListener {
         g.drawString("UPDATE CHANCES", 785, 346);
         g.drawRect((int)updateChancesButton.getX(), (int)updateChancesButton.getY(), (int)updateChancesButton.getWidth(), (int)updateChancesButton.getHeight());
 
-        // Luck and total rolls display
-        g.drawString("Luck: " + rarities.getLuck(), 10, 25);
+        // Luck display and making sure luck doesn't exceed the maximum luck set by the program
+        if (rarities.getLuck() >= MAX_LUCK) {
+            rarities.setLuck(MAX_LUCK);
+            g.drawString("Luck: " + rarities.getLuck() + " (MAX)", 10, 25);
+        }
+        else {
+            g.drawString("Luck: " + rarities.getLuck(), 10, 25);
+        }
+
+        // Total rolls display
         g.drawString("Total rolls: " + totalRolls, 10, 65);
 
         // Displaying rarity chances
@@ -67,15 +76,6 @@ class DrawPanel extends JPanel implements MouseListener {
             g.setFont(new Font("Courier New", Font.PLAIN, 20));
             g.drawString("Rarest rarity obtained: None", 10, 45);
         }
-
-        // Making sure luck doesn't exceed the chance variable of the rarest rarity
-        if (rarities.getLuck() > rarities.getRarities().get(rarities.getRarities().size() - 1).getChance()) {
-            changeLuck(rarities.getRarities().get(rarities.getRarities().size() - 1).getChance());
-        }
-    }
-
-    public void changeLuck(int luck) {
-        rarities.setLuck(luck);
     }
 
     public void mouseReleased(MouseEvent e) { }
@@ -95,16 +95,15 @@ class DrawPanel extends JPanel implements MouseListener {
                 //System.out.println(rarities.raritySimulation(luck, 1000000, "Moonstone"));
                 rolledRarity = rarities.generateRandomRarity(rarities.getLuck());
                 totalRolls++;
+                rarities.setLuck(rarities.getLuck() + rolledRarity.getChance() / 100);
                 if (highestRolledRarity == null) { // No rarity has been rolled yet, therefore no highest rarity
                     highestRolledRarity = rolledRarity;
                     totalRollsForHighestRarity = totalRolls;
-                    changeLuck(rarities.getLuck() + 5);
                 }
                 else {
                     if (rolledRarity.getChance() > highestRolledRarity.getChance()) { // New highest rarity
                         highestRolledRarity = rolledRarity;
                         totalRollsForHighestRarity = totalRolls;
-                        changeLuck(rarities.getLuck() + 5);
                     }
                 }
             } else if (!testLuckButton.contains(clicked)) {
