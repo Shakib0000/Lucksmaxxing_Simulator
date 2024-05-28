b182446d5ad0cc4d32b7daefa42a528acf3de960
Shakib Amin <shakiba4@nycstudents.net>
2024-05-28 14:05:49 -0400

    Rarity chances are now listed in a file to allow for easy configuration, and these chances are set through reading the file.

--- src/Rarities.java
+++ src/Rarities.java
@@ -7,26 +7,28 @@ import java.util.Scanner;
 
 public class Rarities {
     private ArrayList<Rarity> rarities;
+    private ArrayList<Integer> rarityChances;
     private int luck;
 
     public Rarities(int luck) { // LEAST RARE TO MOST RARE
         this.luck = luck;
+        rarityChances = parseRarityChancesFile();
         rarities = new ArrayList<Rarity>();
-        rarities.add(new Rarity(408, 180, "Bronze", new Color(150, 75, 0), new Font("Century Schoolbook", Font.PLAIN, 40), 3));
-        rarities.add(new Rarity(420, 180, "Silver", new Color(192, 192, 192), new Font("Century Schoolbook", Font.PLAIN, 40), 4));
-        rarities.add(new Rarity(430, 180, "Gold", new Color(255, 215, 0), new Font("Century Schoolbook", Font.PLAIN, 40), 7));
-        rarities.add(new Rarity(388, 180, "Diamond", new Color(185, 242, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 60));
-        rarities.add(new Rarity(390, 180, "Emerald", new Color(20, 201, 81), new Font("Century Schoolbook", Font.PLAIN, 40), 105));
-        rarities.add(new Rarity(417, 180, "Topaz", new Color(255, 192, 103), new Font("Century Schoolbook", Font.PLAIN, 40), 125));
-        rarities.add(new Rarity(391, 180, "Sapphire", new Color(15,82,186), new Font("Century Schoolbook", Font.PLAIN, 40), 144));
-        rarities.add(new Rarity(429, 180, "Ruby", new Color(	224, 17, 95), new Font("Century Schoolbook", Font.PLAIN, 40), 188));
-        rarities.add(new Rarity(386, 180, "Amethyst", new Color(125, 0, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 220));
-        rarities.add(new Rarity(362, 180, "Aquamarine", new Color(	127, 255, 212), new Font("Century Schoolbook", Font.PLAIN, 40), 260));
-        rarities.add(new Rarity(432, 180, "Jade", new Color(3, 248, 162), new Font("Century Schoolbook", Font.PLAIN, 40), 310));
-        rarities.add(new Rarity(429, 180, "Opal", new Color(246, 149, 226), new Font("Century Schoolbook", Font.PLAIN, 40), 414));
-        rarities.add(new Rarity(389, 180, "Platinum", new Color(229,228,226), new Font("Century Schoolbook", Font.PLAIN, 40), 463));
-        rarities.add(new Rarity(387, 180, "Titanium", new Color(121, 121, 130), new Font("Century Schoolbook", Font.PLAIN, 40), 635));
-        rarities.add(new Rarity(376, 180, "Moonstone", new Color(190, 211, 255), new Font("Century Schoolbook", Font.PLAIN, 40), 1499));
+        rarities.add(new Rarity(408, 180, "Bronze", new Color(150, 75, 0), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(420, 180, "Silver", new Color(192, 192, 192), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(430, 180, "Gold", new Color(255, 215, 0), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(388, 180, "Diamond", new Color(185, 242, 255), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(390, 180, "Emerald", new Color(20, 201, 81), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(417, 180, "Topaz", new Color(255, 192, 103), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(391, 180, "Sapphire", new Color(15,82,186), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(429, 180, "Ruby", new Color(	224, 17, 95), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(386, 180, "Amethyst", new Color(125, 0, 255), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(362, 180, "Aquamarine", new Color(	127, 255, 212), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(432, 180, "Jade", new Color(3, 248, 162), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(429, 180, "Opal", new Color(246, 149, 226), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(389, 180, "Platinum", new Color(229,228,226), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(387, 180, "Titanium", new Color(121, 121, 130), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
+        rarities.add(new Rarity(376, 180, "Moonstone", new Color(190, 211, 255), new Font("Century Schoolbook", Font.PLAIN, 40), rarityChances.get(rarities.size())));
     }
 
     public Rarity generateRandomRarity(double luck) {
@@ -100,7 +102,30 @@ public class Rarities {
         }
     }
 
+    public ArrayList<Integer> parseRarityChancesFile() {
+        File f = new File("data/RarityChances");
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
 
+        ArrayList<String> fileData = new ArrayList<String>();
+        ArrayList<Integer> rarityChances = new ArrayList<Integer>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        for (int i = 0; i < fileData.size(); i++) {
+            String str = fileData.get(i);
+            int index = str.indexOf("=");
+            rarityChances.add(Integer.parseInt(str.substring(index + 1)));
+        }
+
+        return rarityChances;
+    }
 
     public int getLuck() {
         return luck;
